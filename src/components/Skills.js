553e1c4bab6badccdde3de8589d530cbe40728bf
İhsan Yazarel <ihsanyazarel@hotmail.com>
2023-06-05 05:51:35 +0300

    styling updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,7 +11,7 @@ function Skills() {
   const { lightMode } = useContext(Contexts);
   return (
     <div className={`${lightMode ? "" : "bg-[#252128]"} p-px`}>
-      <div className={`flex flex-col items-center my-32`}>
+      <div className={`flex flex-col items-center my-20`}>
       <div className="flex w-[66%]">
         <h2 className={`w-[40%] text-left ${lightMode ? "text-[#4832D3]" : "text-[#CBF281]"}`}>Skills</h2>
         <div className="flex flex-wrap w-[60%] justify-between">
@@ -20,20 +20,20 @@ function Skills() {
             <p className={`${lightMode ? "text-[#777777]" : "text-[#FFFFFF]"}`}>JAVASCRIPT</p>
           </div>
           <div className="flex items-center w-64  ">
-            <img className="h-32 mr-5 my-3 " src={react} />
-            <p className={`${lightMode ? "text-[#777777]" : "text-[#FFFFFF]"}`}>REACT</p>
-          </div>
-          <div className="flex items-center w-64  ">
-            <img className="h-32 mr-5 my-3 " src={redux} />
-            <p className={`${lightMode ? "text-[#777777]" : "text-[#FFFFFF]"}`}>REDUX</p>
-          </div>
-          <div className="flex items-center w-64  ">
             <img className="h-32 mr-5 my-3 " src={nodeJs} />
             <p className={`${lightMode ? "text-[#777777]" : "text-[#FFFFFF]"}`}>NODE</p>
           </div>
           <div className="flex items-center w-64  ">
+            <img className="h-32 mr-5 my-3 " src={react} />
+            <p className={`${lightMode ? "text-[#777777]" : "text-[#FFFFFF]"}`}>REACT</p>
+          </div>
+          <div className="flex items-center w-64  ">
             <img className="h-32 mr-5 my-3 " src={vsCode} />
             <p className={`${lightMode ? "text-[#777777]" : "text-[#FFFFFF]"}`}>VS CODE</p>
+          </div>
+          <div className="flex items-center w-64  ">
+            <img className="h-32 mr-5 my-3 " src={redux} />
+            <p className={`${lightMode ? "text-[#777777]" : "text-[#FFFFFF]"}`}>REDUX</p>
           </div>
           <div className="flex items-center w-64  ">
             <img className="h-32 mr-5 my-3 " src={figma} />
